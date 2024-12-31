c0d9f099db4c5d6be1d6213fd0c36af3cded9a74
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-31 14:59:43 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,20 +1,106 @@
-BOT_TOKEN = "6914028314:AAG09vTHsH2No_ock1Oqs4x61KJgFEUDJHg"                              # Require restart after changing it while bot running
-OWNER_ID = "6865672542"                               # Require restart after changing it while bot running
-TELEGRAM_API = "2944318"                           # Require restart after changing it while bot running
-TELEGRAM_HASH = "e9746721493a910d201d898b4329da8f"                          # Require restart after changing it while bot running
+# REQUIRED CONFIG
+BOT_TOKEN = "6914028314:AAG09vTHsH2No_ock1Oqs4x61KJgFEUDJHg"
+OWNER_ID = 6865672542
+TELEGRAM_API = 2944318
+TELEGRAM_HASH = "e9746721493a910d201d898b4329da8f"
 # OPTIONAL CONFIG
-USENET_SERVERS = [{'name': 'main', 'host': '', 'port': 5126, 'timeout': 60, 'username': '', 'password': '', 'connections': 8, 'ssl': 1, 'ssl_verify': 2, 'ssl_ciphers': '', 'enable': 1, 'required': 0, 'optional': 0, 'retention': 0, 'send_group': 0, 'priority': 0}]
-JD_EMAIL = "rootedcyber@gmail.com"
-JD_PASS = "Mahphujalam123###"
-DOWNLOAD_DIR = "/usr/src/app/downloads/"    # Require restart after changing it while bot running
-AUTHORIZED_CHATS = "-1002206248347 -1001884618152 -1002083928316"                     # Require restart after changing it while bot running
+USER_SESSION_STRING = ""
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
+CMD_SUFFIX = ""
+AUTHORIZED_CHATS = "-1001884618152"
+SUDO_USERS = ""
+DATABASE_URL = ""
+STATUS_LIMIT = 10
+DEFAULT_UPLOAD = "rc"
+STATUS_UPDATE_INTERVAL = 15
+FILELION_API = ""
+STREAMWISH_API = ""
+EXTENSION_FILTER = ""
+INCOMPLETE_TASK_NOTIFIER = False
+YT_DLP_OPTIONS = ""
+USE_SERVICE_ACCOUNTS = False
+NAME_SUBSTITUTE = ""
+FFMPEG_CMDS = []
+# GDrive Tools
 GDRIVE_ID = "1P2tR0vm3htIH69X0cOxDBvxIgCRDKPDR"
+IS_TEAM_DRIVE = False
+STOP_DUPLICATE = False
 INDEX_URL = "https://mbot.rootedcyber.workers.dev/0:"
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = 0
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+# JDownloader
+JD_EMAIL = "rootedcyber@gmail.com"
+JD_PASS = "Mahhujalam123###"
+# Sabnzbd
+USENET_SERVERS = [
+    {
+        "name": "main",
+        "host": "",
+        "port": 563,
+        "timeout": 60,
+        "username": "",
+        "password": "",
+        "connections": 8,
+        "ssl": 1,
+        "ssl_verify": 2,
+        "ssl_ciphers": "",
+        "enable": 1,
+        "required": 0,
+        "optional": 0,
+        "retention": 0,
+        "send_group": 0,
+        "priority": 0,
+    }
+]
+
 UPSTREAM_REPO = "https://github.com/my-fork-repo/mirror-leech-telegram-bot"
 UPSTREAM_BRANCH = "master"
-#anasty
-USENET_SERVERS = [{'name': 'main', 'host': '', 'port': 5126, 'timeout': 60, 'username': '', 'password': '', 'connections': 8, 'ssl': 1, 'ssl_verify': 2, 'ssl_ciphers': '', 'enable': 1, 'required': 0, 'optional': 0, 'retention': 0, 'send_group': 0, 'priority': 0}]
-# Update
-MEDIA_GROUP = "True"
+# Leech
+LEECH_SPLIT_SIZE = 0
+AS_DOCUMENT = False
+EQUAL_SPLITS = False
+MEDIA_GROUP = Ttue
+USER_TRANSMISSION = False
+MIXED_LEECH = False
+LEECH_FILENAME_PREFIX = ""
 LEECH_DUMP_CHAT = "-1002233071834"
-SUDO_USERS = "6246819990"
+THUMBNAIL_LAYOUT = ""
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = 0
+BASE_URL = ""
+BASE_URL_PORT = 0
+WEB_PINCODE = False
+# Queueing system
+QUEUE_ALL = 0
+QUEUE_DOWNLOAD = 0
+QUEUE_UPLOAD = 0
+# RSS
+RSS_DELAY = 600
+RSS_CHAT = ""
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = 0
+SEARCH_PLUGINS = [
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+    "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
+    "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+    "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+    "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py",
+]

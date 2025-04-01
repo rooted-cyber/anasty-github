077c78fc913859418f08be19da0617267526f6ea
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-04-02 00:52:15 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -4,38 +4,39 @@ OWNER_ID = 6865672542
 TELEGRAM_API = 2944318
 TELEGRAM_HASH = "e9746721493a910d201d898b4329da8f"
 # OPTIONAL CONFIG
-#USER_SESSION_STRING = ""
-DOWNLOAD_DIR = "/usr/src/app/downloads/"
-#CMD_SUFFIX = ""
+TG_PROXY = {}
+USER_SESSION_STRING = ""
+CMD_SUFFIX = ""
 AUTHORIZED_CHATS = "-1001884618152"
-#SUDO_USERS = ""
-#DATABASE_URL = ""
-STATUS_LIMIT = 10
-DEFAULT_UPLOAD = "gd"
+SUDO_USERS = ""
+DATABASE_URL = ""
+STATUS_LIMIT = 4
+DEFAULT_UPLOAD = "rc"
 STATUS_UPDATE_INTERVAL = 15
-#FILELION_API = ""
-#STREAMWISH_API = ""
-#EXTENSION_FILTER = ""
+FILELION_API = ""
+STREAMWISH_API = ""
+EXCLUDED_EXTENSIONS = ""
 INCOMPLETE_TASK_NOTIFIER = False
-#YT_DLP_OPTIONS = ""
+YT_DLP_OPTIONS = ""
 USE_SERVICE_ACCOUNTS = False
-#NAME_SUBSTITUTE = ""
-FFMPEG_CMDS = []
+NAME_SUBSTITUTE = ""
+FFMPEG_CMDS = {}
+UPLOAD_PATHS = {}
 # GDrive Tools
 GDRIVE_ID = "1P2tR0vm3htIH69X0cOxDBvxIgCRDKPDR"
 IS_TEAM_DRIVE = False
 STOP_DUPLICATE = False
 INDEX_URL = "https://mbot.rootedcyber.workers.dev/0:"
 # Rclone
-#RCLONE_PATH = ""
-#RCLONE_FLAGS = ""
-#RCLONE_SERVE_URL = ""
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
 RCLONE_SERVE_PORT = 0
-#RCLONE_SERVE_USER = ""
-#R##CLONE_SERVE_PASS = ""
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
 # JDownloader
 JD_EMAIL = "rootedcyber@gmail.com"
-JD_PASS = "Mahhujalam123###"
+JD_PASS = "Mahphujalam123###"
 # Sabnzbd
 USENET_SERVERS = [
     {
@@ -57,22 +58,22 @@ USENET_SERVERS = [
         "priority": 0,
     }
 ]
-
-UPSTREAM_REPO = ""
-UPSTREAM_BRANCH = ""
+# Update
+UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+UPSTREAM_BRANCH = "master"
 # Leech
 LEECH_SPLIT_SIZE = 0
 AS_DOCUMENT = False
 EQUAL_SPLITS = False
-#MEDIA_GROUP = True
+MEDIA_GROUP = False
 USER_TRANSMISSION = False
-MIXED_LEECH = False
-#LEECH_FILENAME_PREFIX = ""
-#LEECH_DUMP_CHAT = "-1002233071834"
-#THUMBNAIL_LAYOUT = ""
+HYBRID_LEECH = False
+LEECH_FILENAME_PREFIX = ""
+LEECH_DUMP_CHAT = ""
+THUMBNAIL_LAYOUT = ""
 # qBittorrent/Aria2c
 TORRENT_TIMEOUT = 0
-#BASE_URL = ""
+BASE_URL = ""
 BASE_URL_PORT = 0
 WEB_PINCODE = False
 # Queueing system
@@ -81,13 +82,11 @@ QUEUE_DOWNLOAD = 0
 QUEUE_UPLOAD = 0
 # RSS
 RSS_DELAY = 600
-#RSS_CHAT = ""
+RSS_CHAT = ""
+RSS_SIZE_LIMIT = 0
 # Torrent Search
-#SEARCH_API_LINK = ""
+SEARCH_API_LINK = ""
 SEARCH_LIMIT = 0
-FFMPEG_CMDS = {}
-UPLOAD_PATHS = {}
-TG_PROXY = {}
 SEARCH_PLUGINS = [
     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
